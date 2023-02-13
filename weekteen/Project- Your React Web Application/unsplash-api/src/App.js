54bb88d22c0af0ca4c013493ef4fa6acc0ab0a50
Igor Savin <remotor-work@yandex.ru>
2023-02-13 10:34:30 -0500

    added shadow effect

--- weekteen/Project- Your React Web Application/unsplash-api/src/App.js
+++ weekteen/Project- Your React Web Application/unsplash-api/src/App.js
@@ -7,13 +7,40 @@ import Header from './Header';
 import './Cards.css'
 import './Card.css'
 
+let Input = {
+  boxSizing: 'border-box',
+  paddingLeft: '10px',
+  color: '#000',
+  width: '100%',
+  maxWidth: '684px',
+  height: '54px',
+  fontSize: '14px',
+  border: '1px solid #c4c4c4',
+  borderRadius: '5px',
+  margin: '10px',
+};
+
 function App() {
   let [inputValue, setInputValue] = useState('');
   let [cardsData, setcardsData] = useState([]);
   const inputRef = useRef(false);
 
   useEffect(() => {
-    console.log('chenched', inputValue);
+    console.log('chenched', inputValue, inputValue.length);
+    Input = {
+      boxShadow: `0 0 ${inputValue.length}px ${inputValue.length}px	rgba(82, 153, 224, 0.5)`,
+      boxSizing: 'border-box',
+      paddingLeft: '10px',
+      color: '#000',
+      width: '100%',
+      maxWidth: '684px',
+      height: '54px',
+      fontSize: '14px',
+      border: `1px solid #48abe0`,
+      borderRadius: '5px',
+      margin: '10px',
+      outline: '0',
+    }
   }, [inputValue])
 
   const handleSubmit = (e) => {
@@ -38,17 +65,18 @@ function App() {
     handleSubmit(e);
   }
 
+
   return (
     <div className="App">
       <Header />
       <form className='App__form' onSubmit={handleSubmit}>
-        <input onChange={handleInput} value={inputValue} ref={inputRef} className="Input" type="text" placeholder="Search free high-resolution photos" />
+        <input className='Input' style={Input} maxLength="20" onChange={handleInput} value={inputValue} ref={inputRef} type="text" placeholder="Search free high-resolution photos" />
         <button className="Button" >Search</button>
       </form>
       <div className='Cards'>
         {
           cardsData.map((card, index) => (
-            <div className='Card'>
+            <div className='Card' key={index}>
               <img className='Card-image' src={card.link} alt="" />
               <h3 className='Card-title'>{card.title}</h3>
               <p className='Card-subtitle'>{card.subtitle}</p>
